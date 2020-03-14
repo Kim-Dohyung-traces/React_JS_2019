40f5ab3771ae7d943ab3f76fcf4517bf12431aa8
김도형 <sdkdh168@gmail.com>
2020-03-14 15:43:06 +0900

    4th commit jsx + props Fundamentals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Pizza from './Pizza';
+function Pizza({ menu }) {
+  console.log(menu)
+  return <h3>this {menu}</h3>
+}
 
 function App() {
   return (
     <div>
       <h1>notting...</h1>
-      <Pizza />
+      <Pizza menu="Margherita Pizza" />
+      <Pizza menu="Pepperoni Pizza" />
+      <Pizza menu="Potato Pizza" />
+      <Pizza menu="Cheese Pizza" />
     </div>
-    );
+  );
 }
 
 export default App;
